6c50cc4f60d6736a0c10c02cbc3779c67236458a
janc18 <josecla011@gmail.com>
2023-10-20 21:46:33 -0600

    chore: se agregan algunos mensajes predefinidos para mostrarlos en el futuro dependiendo de la situacion

--- Herramienta_de_instalacion/mensajes.py
+++ Herramienta_de_instalacion/mensajes.py
@@ -1,6 +1,10 @@
 """ 
 Aqui se mostraran todos los mensajes para el usuario
-
 """
 #intrucciones de proceso
 #que hacer dependiendo del analisis de todo los archivos encontrados
+mensaje_bienvenida="Ejecutando script para la instalación del driver RTL8188FTV para la ARM"
+nota_distro_compatible="Este script es solo compatible con distros basadas en Debian"
+kernel_no_compatible="La version del Kernel de ARM no es compatible con el driver"
+version="v1.0.0"
+analisis="Ejecutando analisis para proceder de la manera mas adecuada"
