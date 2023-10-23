6c994f98dd687b6c158533cec769bf6271d7eeb7
janc18 <josecla011@gmail.com>
2023-10-23 17:46:10 -0600

    update: se termina de desarrollar la funcion que descarga los paquetes y repositorio en el dispositivo que cuenta con internet, ademas de remplazar la matriz de status por un diccionario

--- Herramienta_de_instalacion/analisis.py
+++ Herramienta_de_instalacion/analisis.py
@@ -21,12 +21,13 @@ con un resumen de datos como: conexion de internet, arquitectura, y depencias en
 
 
 import mostrar_informacion as st #st= status
+import descargador as dw
 
 def generar_txt_con_uris_de_paquetes(lista_resumen):#Esta funcion solo se debe de ejecutar en el dispositivo con arquitectura ARM
-    arquitectura_arm=lista_resumen[0]
-    kernel_compatible=lista_resumen[1]
-    cuenta_con_apt=lista_resumen[3]
-    existe_el_archivo_con_links=lista_resumen[6]
+    arquitectura_arm=lista_resumen["arquitectura"]
+    kernel_compatible=lista_resumen["kernel_compatible"]
+    cuenta_con_apt=lista_resumen["existe_apt"]
+    existe_el_archivo_con_links=lista_resumen["existe_archivos_links"]
     mensajes_de_errores=[
         "La arquitectura no es ARM",
         "La versión del Kernel no es compatible con el driver",
@@ -42,11 +43,11 @@ def generar_txt_con_uris_de_paquetes(lista_resumen):#Esta funcion solo se debe d
     
 
     if arquitectura_arm and kernel_compatible and cuenta_con_apt and not existe_el_archivo_con_links:
-        st.obtener_links_de_descargas
+        st.obtener_links_de_descargas()
     elif  arquitectura_arm and kernel_compatible and cuenta_con_apt and existe_el_archivo_con_links:
-        generar_links_de_descargas=input("Ya se cuenta con el archivo de links, quieres usar ese archivo o generar uno nuevo\n[s/S] [n/N]")
+        generar_links_de_descargas=input("Ya se cuenta con el archivo de links, quieres generar uno nuevo\n[s/S] [n/N]\n")
         if generar_links_de_descargas in ("S","s"):
-            st.main.obtener_links_de_descargas
+            st.obtener_links_de_descargas()
         elif generar_links_de_descargas in ("N","n"):
             print("Usando archivo antiguo")
         else:
@@ -57,7 +58,20 @@ def generar_txt_con_uris_de_paquetes(lista_resumen):#Esta funcion solo se debe d
                 print(st.ROJO,end="")
                 print(mensajes_de_errores[error])
                 print(st.NORMAL,end="")
-
 def descargar_todos_los_componentes_necesarios(lista_resumen):
-    if lista_resumen[4]==False:
+    if lista_resumen["existe_conexion_internet"]==False:
         print("No se cuenta con internet para realizar el proceso")
+        exit()
+    if lista_resumen["existe_archivos_links"]==False:
+        print("No existe el \"archivos_links.txt\", no es posible realizar la descarga")
+        exit()
+    if lista_resumen["existe_repositorio_driver"]==False: 
+        dw.descargar_repositorio_git(dw.url_repositorio_git_driver,lista_resumen)
+
+    if lista_resumen["existe_archivos_links"] and not lista_resumen["existe_paquetes_descargados"]:
+        lista_formateada=dw.formatear_archivo_links()
+        dw.descargar_archivos_deb_con_requests(lista_formateada)
+        dw.mover_descargas_deb_a_directorio(".",dw.ruta_de_carpeta_de_descargas)
+        dw.verificando_integridad_sha256(lista_formateada)
+    if lista_resumen["existe_paquetes_descargados"] and lista_resumen["existe_repositorio_driver"]:
+        return True
