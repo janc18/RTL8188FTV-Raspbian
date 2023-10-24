df274ca4558b241536fad1c6058cfed8893edd67
janc18 <josecla011@gmail.com>
2023-10-24 17:43:37 -0600

    update: se agrega instrucciones extras en archivo Instalador.sh para cambiar a la rama arm

--- Herramienta_de_instalacion/Instalador.sh
+++ Herramienta_de_instalacion/Instalador.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #Instalación de todos los paquetes necesarios .deb
-sudo dpkg -i **.deb
+sudo dpkg -i Descargas_de_paquetes/**.deb
+#Hacer checkout al ultimo commit donde tenia soporte la arquitectura arm
+cd rtl8188fu
+git checkout b037
+git checkout arm
+cd ..
 #Instalación del driver
 sudo ln -s /lib/modules/$(uname -r)/build/arch/arm /lib/modules/$(uname -r)/build/arch/armv7l
-sudo dkms add ./rtl8188fu-arm
+sudo dkms add ./rtl8188fu
 sudo dkms build rtl8188fu/1.0
 sudo dkms install rtl8188fu/1.0
 sudo cp ./rtl8188fu-arm/firmware/rtl8188fufw.bin /lib/firmware/rtlwifi/
